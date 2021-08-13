c851d7282fc951b79a94455f1bacdda457227166
Matheus Bortolucci <matheusbzo@hotmail.com>
2021-08-13 01:27:30 -0300

    ✨Corrigido sistema Strike

--- commands/CreatorsOnly/strike.js
+++ commands/CreatorsOnly/strike.js
@@ -17,40 +17,46 @@ module.exports = class strike extends Command {
         }
         switch (args[0]) {
             case "add": {
-                let user = await this.client.users.fetch(args[1])
-                let userDB = await this.client.database.Users.findById(user.id)
+                const usuario = await this.client.users.fetch(args[1].replace(/[<@!>]/g, ""))
+                if (!usuario) {
+                    message.channel.send('Mencione um membro valido.')
+                }
+                const guildDocument = await this.client.database.Users.findById(usuario.id)
+                if (!guildDocument) {
+                    new this.client.database.Users({
+                        _id: usuario.id
+                    }).save()
+                }
                 const strike = new MessageEmbed()
 
-                    .setThumbnail(user.displayAvatarURL({ dynamic: true, size: 1024 }))
+                    .setThumbnail(usuario.displayAvatarURL({ dynamic: true, size: 1024 }))
                     .setTitle('System | Trust & Safety')
                     .setColor(colors.default)
-                    .setDescription(`\n\n<:a_blurpleemployee:856174396423274516> O Discord é focado em manter um conteúdo seguro e confiável para nossa comunidade, e sua conta foi sinalizada pela comunidade do Discord por violar nossas Diretrizes da Comunidade.\n\n<:Kaeltec:673592197177933864> **Staff:** ${message.author.username} \n**ID:** ${message.author.id}` + `\n<:Kaeltec:673592197177933864> **Infrator:** ${user.username} \n**ID:** ${user.id}`)
+                    .setDescription(`\n\n<:a_blurpleemployee:856174396423274516> O Discord é focado em manter um conteúdo seguro e confiável para nossa comunidade, e sua conta foi sinalizada pela comunidade do Discord por violar nossas Diretrizes da Comunidade.\n\n<:Kaeltec:673592197177933864> **Staff:** ${message.author.username} \n**ID:** ${message.author.id}` + `\n<:Kaeltec:673592197177933864> **Infrator:** ${usuario.username} \n**ID:** ${usuario.id}`)
                     .setImage(`https://miro.medium.com/max/3200/0*SCBRci_mo1Yhc9km`)
                     .setFooter("https://discordapp.com/guidelines・Discord da Jeth", message.guild.iconURL({ dynamic: true, size: 1024 }))
                     .setTimestamp(new Date());
-                userDB.strike += 1
-                userDB.save().then(() => message.channel.send(strike))
-                if (userDB.strike >= 3) {
-                    userDB.blacklist = true
-                    userDB.blacklistReason = "[STRIKE EXCEDIDO] - Quebra dos termos de uso do Discord."
-                    userDB.save().then(() => message.channel.send(`O usuário \`${user.tag}\` foi adicionado da blacklist.`))
+                guildDocument.strike += 1
+                guildDocument.save().then(() => message.channel.send(strike))
+                if (guildDocument.strike >= 3) {
+                    guildDocument.blacklist = true
+                    guildDocument.blacklistReason = "[STRIKE EXCEDIDO] - Quebra dos termos de uso do Discord."
+                    guildDocument.save().then(() => message.channel.send(`O usuário \`${usuario.tag}\` foi adicionado da blacklist.`))
                 }
-                message.channel.send(`O usuário \`${user.tag}\` acabou levando strike (${userDB.strike}))`)
-                user.send('Olá, \n\nO Discord é focado em manter um conteúdo seguro e confiável para nossa comunidade, e sua conta foi sinalizada pela comunidade do Discord por violar nossas Diretrizes da Comunidade.\n\nSinceramente,\nDiscord Trust & Safety Team')
+                message.channel.send(`O usuário \`${usuario.tag}\` acabou levando strike (${guildDocument.strike}))`)
+                usuario.send('Olá, \n\nO Discord é focado em manter um conteúdo seguro e confiável para nossa comunidade, e sua conta foi sinalizada pela comunidade do Discord por violar nossas Diretrizes da Comunidade.\n\nSinceramente,\nDiscord Trust & Safety Team')
             }
                 break
             case "remove": {
-                let user = await this.client.users.fetch(args[1])
-                let userDB = await this.client.database.Users.findById(user.id)
-                message.channel.send(`${user.tag} Teve seu strike removido! <:a_blurplecertifiedmoderator:856174396225355776>`)
-                userDB.strike -= 1
-                userDB.save()
-                if (userDB.strike < 3) {
-                    userDB.blacklist = false
-                    userDB.blacklistReason = null
-                    userDB.save().then(() => message.channel.send(`O usuário \`${user.tag}\` foi removido da blacklist.`))
+                message.channel.send(`${usuario.tag} Teve seu strike removido! <:a_blurplecertifiedmoderator:856174396225355776>`)
+                guildDocument.strike -= 1
+                guildDocument.save()
+                if (guildDocument.strike < 3) {
+                    guildDocument.blacklist = false
+                    guildDocument.blacklistReason = null
+                    guildDocument.save().then(() => message.channel.send(`O usuário \`${usuario.tag}\` foi removido da blacklist.`))
                 }
-                message.channel.send(`O usuário \`${user.tag}\` teve 1 strike removido (${userDB.strike})`)
+                message.channel.send(`O usuário \`${usuario.tag}\` teve 1 strike removido (${guildDocument.strike})`)
             }
                 break
             default: {
