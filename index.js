e947e723988ea5964fddef2a99847abf20e47931
DanielaGC <danielagc676@gmail.com>
2021-08-25 01:13:17 -0300

    Add intents

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 
 const JethCanary = require('./JethCanary')
-const client = new JethCanary({ fetchAllMembers: true })
+const client = new JethCanary({ fetchAllMembers: true, ws: { intents: 14023 } })
 const TopGG = require("dblapi.js")
 const top = new TopGG(process.env.TOPGG, client)
-top.on("error", (err) => console.log(err))
+top.on("error", (err) => console.log(err.name))
 top.on("posted", () => console.log("Contador de servidores públicado!"))
 client.login(process.env.TOKEN)
-.then(() => {
-    console.log('Online!')
-    console.log(process.memoryUsage().rss /512 / 512)
-})
-.catch(err => {
-    console.log(`Falha ao iniciar o bot :::: ${err}`)
-})
+    .then(() => {
+        console.log('Online!')
+        console.log(process.memoryUsage().rss / 512 / 512)
+    })
+    .catch(err => {
+        console.log(`Falha ao iniciar o bot :::: ${err}`)
+    })
