d98819e266dc9b769aafec353ba6a56e26fdb8ae
Matheus Bortolucci <matheusbzo@hotmail.com>
2021-12-20 23:25:47 -0500

    🐞 Correções gerais de bug.

--- commands/Moderação/softban.js
+++ commands/Moderação/softban.js
@@ -12,7 +12,7 @@ module.exports = class softban extends Command {
 
     async run(message, args) {
         var cor = '#c635ff'
-        let usuario = bot.users.cache.get(args[0]) || message.mentions.users.cache.first()
+        let usuario = this.client.users.cache.get(args[0]) || message.mentions.users.cache.first()
         if (!message.guild.member(message.author.id).hasPermissions("BAN_MEMBERS")) return message.reply(":x: **|** Você não tem permissão para executar este comando!")
         if (message.mentions.users.size < 1) return message.reply('Mencione algum membro')
         if (!message.guild.member(usuario).bannable) return message.reply(`:x: **|** Eu não posso punir essa pessoa, talvez o cargo dela seja maior que o meu`)
