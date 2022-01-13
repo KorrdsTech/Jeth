001402619948d1a1955ccb9da788ec4d1a76cb92
Matheus Bortolucci <matheusbzo@hotmail.com>
2022-01-12 23:42:24 -0300

    🐞 Bug
    Palavras separadas fazem com que se encontrem erros no console "seu preto", "sua preta"
    correção adicionada foi "seupreto", "suapreta"
    desta forma mesmo que as palavras sejam colocadas no chat separadamente o .split irá identifica-las e remover a mensagem enviando o aviso :3

--- listeners/message.js
+++ listeners/message.js
@@ -140,7 +140,7 @@ module.exports = async function onMessage(message, client, messageDelete, msg) {
 // block words
 let guild = await this.database.Guilds.findById('804575416098488380')
 if(message.guild.id === guild.id){
-    const forbiddenWords = ["corno", "g4do", "g4d0", "c0rno","gado", "c0rn0", "macaco", "preto", "lesbica", "lésbica", "gay", "viado", "seu preto", "sua preta", "macaca", "buceta"]
+    const forbiddenWords = ["corno", "g4do", "g4d0", "c0rno","gado", "c0rn0", "macaco", "preto", "lesbica", "lésbica", "gay", "viado", "seupreto", "suapreta", "macaca", "buceta"]
     if(message.content.split(" ").some(wordMsg => forbiddenWords.includes(wordMsg))){
         message.delete({timeout: 5})
         let mensagem = `${message.author} <:a_angyy:924250381108785192> Você utilizou uma ou mais palavras ofensivas em sua mensagem, para a segurança de nossa comunidade, ela foi apagada! se continuar com um comportamento tóxico será banido de nossa comunidade.`
