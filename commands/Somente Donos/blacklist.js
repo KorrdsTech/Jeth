7392576691393e89332afdd6afd004c6242e478f
Matheus Bortolucci <matheusbzo@hotmail.com>
2022-01-19 19:54:36 -0300

    Revert "Só aceita após conclusão do #46 (#47)"
    This reverts commit de3ccc4c07cd9c65bbe8acc134c51c83f55d22b5.

--- commands/Somente Donos/blacklist.js
+++ commands/Somente Donos/blacklist.js
@@ -18,17 +18,17 @@ module.exports = class blacklist extends Command {
         if (!args[0]) {
             return message.channel.send('Você tem que falar o id do usuario para que eu póssa adicionar na blacklist...').then(sent => sent.delete({ timeout: 5000 }))
         }
-        const usuario = await this.client.users.fetch(args[0].replace(/[<@!>]/g, ""))
+        let usuario = await this.client.users.fetch(args[0].replace(/[<@!>]/g, ""))
         if (!usuario) {
             message.channel.send('Mencione um membro valido.')
         }
-        const guildDocument = await this.client.database.Users.findById(usuario.id)
+        let guildDocument = await this.client.database.Users.findById(usuario.id)
         if (!guildDocument) {
             new this.client.database.Users({
                 _id: usuario.id
             }).save()
         }
-        const reason = args.slice(1).join(" ")
+        let reason = args.slice(1).join(" ")
         if (!reason) {
             reason = "Qual o motivo da blacklist?"
         }
@@ -36,7 +36,7 @@ module.exports = class blacklist extends Command {
         let warnembed18 = new MessageEmbed()
 
             .setThumbnail(message.guild.iconURL({ dynamic: true, size: 1024 }))
-            .setTitle(`${message.author.username}`)
+            .setTitle(`${message.author}`)
             .setDescription(`:do_not_litter: **Você foi blacklisted ${message.guild.name} :no_entry_sign:** \nO que isto significa ? você não poderá mais fazer parte dos servidores que apoiam a network da Jeth, por quebrar um dos termos de serviço do discord, este tipo de punição não oferece appeal e se você se encontra nesta situação provavelmente terá sua conta encerrada.`)
             .setColor("BLACK")
             .addField('<:pepe:651487933148299291> **Staffer:**', `${message.author}`)
