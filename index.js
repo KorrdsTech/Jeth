7b4bb5907e4bc672f648fe1c17d221a3de85a7f4
DanielaGC <danielagc676@gmail.com>
2022-02-01 20:07:19 -0300

    Fix `MISSING_TOKEN`'s error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js')
 require('dotenv').config()
-const shard = new ShardingManager('./src/JethLauncher.js', { totalShards: Number(process.env.TOTAL_SHARDS) ?? 1, respawn: true })
+const shard = new ShardingManager('./src/JethLauncher.js', { totalShards: Number(process.env.TOTAL_SHARDS) ?? 1, respawn: true, token: process.env.TOKEN })
 
 shard.spawn()
