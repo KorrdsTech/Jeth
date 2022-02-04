aaa342aaa2986bb487b54c171f1ed2655953f83a
Matheus Bortolucci <matheusbzo@hotmail.com>
2022-02-04 12:03:56 -0300

    Fixing Badges Duplicatas - Closes #44 (#58)
    Added new badges to userinfo and increased high performance, removing several junks from inside the whole code.

--- commands/Miscellaneous/userinfo.js
+++ commands/Miscellaneous/userinfo.js
@@ -13,30 +13,75 @@ module.exports = class userinfo extends Command {
 	}
 
 	async run(message, args, user) {
-		// const status = {
-		// 	online: {
-		// 		msg: `Online`,
-		// 		color: "00C903"
-		// 	},
-		// 	idle: {
-		// 		msg: `Ausente`,
-		// 		color: "FF9A00"
-		// 	},
-		// 	dnd: {
-		// 		msg: `Não incomodar`,
-		// 		color: "FF0000"
-		// 	},
-		// 	offline: {
-		// 		msg: `Desconectado/invisivel`,
-		// 		color: "D8D8D8"
-		// 	},
-		// };
 
-		let pUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author)
+
+		const jeth = this.client.guilds.cache.get('804575416098488380')
+		const pUser = message.guild.members.cache.get(args[0]?.replace(/[<@!>]/g, '') || message.author.id)
+		const flags = []
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('838581046881681419')) {
+		  flags.push('<:CEO:938280523585957978>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('831041495326261278')) {
+		  flags.push('<:StaffJeth:938280523447566376>')
+		}
+	}
+
+	if (jeth.members.cache.get(pUser.user.id)) {
+		if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('804590005577842689')) {
+		  flags.push('<:ServerBooster:938280524047343656>')
+		}
+	}
+
+	if (jeth.members.cache.get(pUser.user.id)) {
+		if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('839754099573522452')) {
+		  flags.push('<:UsuarioVIP:938280523468509245>')
+		}
+	}
+	
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('838921340085731338')) {
+		  flags.push('<:BugHunter:938280523426578534>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('838994687985451039')) {
+		  flags.push('<:ParceiroJeth:938282680028651600>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('927895349769687080')) {
+			flags.push('<:Bravery:938280523153965097>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('927895430124159037')) {
+			flags.push('<:Balance:938280523409817600>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('927895419466420254')) {
+			flags.push('<:Brilliance:938280523426578533>')
+		}
+	}
+
+		if (jeth.members.cache.get(pUser.user.id)) {
+			if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('838650358342352927')) {
+			flags.push('<:TrustSafety:938280523091025964>')
+		}
+	}
 
 		let normalUser = new Discord.MessageEmbed()
 			.setAuthor(pUser.user.tag, pUser.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-			.addField(`Discord Tag`, `**${pUser.user.tag}**`, true)
+			.addField(`Discord Tag`, `${flags?.join(' ')} \n**${pUser.user.tag}**`, true)
 			.addField("Status", `\`\`\`md\n# ${pUser.presence.status} \`\`\``, true)
 			.addField("Jogando", `\`\`\`md\n# ${pUser.user.presence.game ? `${pUser.presence.game.name}` : "Nada"}\`\`\``, false)
 			.addField(`Discord Name`, `\`\`\`diff\n- ${pUser.user.username} -\`\`\``, true)
@@ -51,72 +96,6 @@ module.exports = class userinfo extends Command {
 			.setThumbnail(pUser.user.displayAvatarURL({ dynamic: true, size: 1024 }))
 			.setFooter(`${message.guild.name} - ${moment().format("LL")}`, message.guild.iconURL({ dynamic: true, size: 1024 }));
 
-			let deviants = new Discord.MessageEmbed()
-			.setAuthor(pUser.user.tag, pUser.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-			.addField(`Discord Tag`, `**${pUser.user.tag}**`, true)
-			.addField("Status", `\`\`\`md\n# ${pUser.presence.status} \`\`\``, true)
-			.addField("Jogando", `\`\`\`md\n# ${pUser.user.presence.game ? `${pUser.presence.game.name}` : "Nada"}\`\`\``, false)
-			.addField(`Discord Name`, `\`\`\`diff\n- ${pUser.user.username} -\`\`\``, true)
-			.addField('ID', `\`\`\`\n${pUser.id}\`\`\``, true)
-			.addField(`Apelido dentro do servidor`, pUser.nickname ? pUser.nickname : "`Nenhum apelido neste servidor.`", true)
-			.addField(`Conta criada em`, `\`${moment(pUser.user.createdTimestamp).format("LL")}\``, true)
-			.addField(`Dias no Discord:`, `Estou á \`${moment().diff(pUser.user.createdAt, "days")}\` dia (s) no discord`, true)
-			.addField(`Dias no servidor:`, `Estou á \`${moment().diff(pUser.joinedAt, "days")}\` dia (s) no servidor`, true)
-			.addField('🌎 | Servidores compartilhados:', `${this.client.guilds.cache.filter(a => a.members.cache.get(pUser.user.id)).map(a => a.name).join(', ')}`, false)
-			.setColor(colors.default)
-			.setThumbnail(pUser.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-			.setFooter(`${message.guild.name} - ${moment().format("LL")}`, message.guild.iconURL({ dynamic: true, size: 1024 }));
-{
-		let roll = pUser.roles.cache.map(role => role.toString()).join(" ").replace('@everyone', ' ')
-		if(!roll.includes('831041498870710313')) message.channel.send(deviants)
-}
-		let embed = new Discord.MessageEmbed()
-			.setDescription('**Veja todas as insígnias que este usuário possui dentro da Jeth:**\n\nCaso você não esteja vendo nenhuma insígnia, certifique-se de estar visualizando pelo nosso servidor oficial da Jeth, caso mesmo assim elas não estejam sendo aparecendo utilize /bug')
-			.setColor(colors.default)
-			.setThumbnail(pUser.user.displayAvatarURL({ dynamic: true, size: 1024 }))
-			.setFooter(`${message.guild.name} - ${moment().format("LL")}`, message.guild.iconURL({ dynamic: true, size: 1024 }));
-
-
-		await message.channel.send(normalUser).then(msg => {
-			setTimeout(() => {
-				msg.react('856174396036743230')
-			}, 500)
-			const collector = msg.createReactionCollector((r, u) => (r.emoji.id === '856174395801468989', '856174396129148968') && (u.id !== this.client.user.id && u.id === message.author.id))
-			collector.on('collect', async r => {
-				switch (r.emoji.id) {
-					case '856174396036743230':
-						if (pUser.roles.cache.has('838581046881681419')) {
-							embed.addField('<:e_king:879546953787138068>', '**This member is the bot Owner.**')
-						}
-						if (pUser.roles.cache.has('831041495326261278')) {
-							embed.addField('<:a_blurpleemployee:856174396423274516>', '**This user is a staff member.**')
-						}
-						if (pUser.roles.cache.has('804590005577842689')) {
-							embed.addField('<a:0_:875581760262504468>', '**This user is a server booster.**')
-						}
-						if (pUser.roles.cache.has('839754099573522452')) {
-							embed.addField('<:a_blurplegift:856174396384215040>', '**This user is a vip member.**')
-						}
-						if (pUser.roles.cache.has('838921340085731338')) {
-							embed.addField('<:e_bug_hunter_lvl2:879567683182538783>', '**This user reported bot bugs for the devs.**')
-						}
-						if (pUser.roles.cache.has('838994687985451039')) {
-							embed.addField('<:a_blurplepartner:856174395869626399>', '**This user is a Jeth partner.**')
-						}
-						await msg.edit(embed)
-						msg.reactions.cache.get("856174396036743230").users.remove();
-						msg.reactions.cache.get("856174396036743230").users.remove(pUser);
-						msg.react('856174395801468989')
-						break;
-					case '856174395801468989':
-						await msg.edit(normalUser)
-						msg.reactions.cache.get('856174395801468989').users.remove();
-						msg.reactions.cache.get('856174395801468989').users.remove(pUser);
-						msg.react('856174396036743230')
-						break;
-
-				}
-			})
-		})
+		message.channel.send(normalUser)
 	}
 }
