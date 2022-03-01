379c242a250722f31d1b45e920739f7ab9b88549
RootGC_ <matheusbzo@hotmail.com>
2022-03-01 03:18:15 -0300

    Chaning how serverinfo works

--- src/commands/Misc/ServerInfoCommand.js
+++ src/commands/Misc/ServerInfoCommand.js
@@ -12,6 +12,20 @@ module.exports = class serverinfo extends Command {
   }
 
   async run(message) {
+    const filterLevels = {
+      DISABLED: 'Off',
+      MEMBERS_WITHOUT_ROLES: 'No Role',
+      ALL_MEMBERS: 'Everyone'
+    };
+
+    const verificationLevels = {
+      NONE: 'None',
+      LOW: 'Low',
+      MEDIUM: 'Medium',
+      HIGH: '(╯°□°）╯︵ ┻━┻',
+      VERY_HIGH: '┻━┻ ﾐヽ(ಠ益ಠ)ノ彡┻━┻'
+    };
+
     if (!message.guild.me.permissions.has('SEND_MESSAGES')) return console.log('DISCORD: Estou sem permissão em um servidor.')
     const embed = new MessageEmbed()
 
@@ -19,7 +33,7 @@ module.exports = class serverinfo extends Command {
       .setColor(colors.default)
       .setThumbnail(message.guild.iconURL({ dynamic: true, size: 1024 }))
       .setTitle('🧭 **Informações do Servidor:**')
-      .setDescription(`🙋🏻 **| Nome:** \n${message.guild.name} \n \n 👑 **| Dono:** \n ${message.guild.owner.user} \n \n  🤹🏼‍♂️ **| Membros:** \n ${message.guild.memberCount} \n \n 🗺 **| Região do Servidor:** \n ${message.guild.region} \n \n ⌛️ **| Criado:** \n ${message.guild.createdAt}`) // inline false
+      .setDescription(`🙋🏻 **| Nome:** \n${message.guild.name} \n \n  🤹🏼‍♂️ **| Membros:** \n ${message.guild.memberCount} \n \n ⌛️ **| Criado:** \n ${message.guild.createdAt} \n \n🔐 ** | Nível de verificação:** ${verificationLevels[message.guild.verificationLevel]} \n \n🔞 ** | Filtro de conteúdo explícito:** ${filterLevels[message.guild.explicitContentFilter]}`) // inline false
       .setFooter('🧁・Discord da Jeth', message.author.displayAvatarURL({ dynamic: true, size: 1024 }))
 
     message.channel.send({ embeds: [embed] })
