74826b9118fc460fccb88cf74fca743504219928
RootGC_ <matheusbzo@hotmail.com>
2022-02-28 21:26:04 -0300

    Cleaning and fixing MuteCommand

--- src/commands/Moderação/MuteCommand.js
+++ src/commands/Moderação/MuteCommand.js
@@ -1,5 +1,5 @@
 const { Command, colors } = require('../../utils')
-const Discord = require('discord.js')
+const { Discord } = require('discord.js')
 const parse = require('parse-duration')
 
 module.exports = class mute extends Command {
@@ -14,23 +14,43 @@ module.exports = class mute extends Command {
 
   async run(message, args) {
     const member = message.mentions.members.first() || message.guild.members.cache.get(args[1]);
-    const embedA = new Discord.MessageEmbed()
+
+    const time = args[1];
+
+    const erroDePermissão = new Discord.MessageEmbed()
       .setTimestamp()
       .setColor(colors.mod)
       .setTitle('**Err:**', `${member}`, true)
-      .setDescription('Missing Permissions') // inline false
+      .setDescription('Missing Permissions')
       .addField('*Verifique se você possui a permissão:*', '`KICK_MEMBERS`', true)
       .setFooter('🧁・Discord da Jeth', message.guild.iconURL({ dynamic: true, size: 1024 }))
-    if (!message.member.hasPermission('KICK_MEMBERS'))
-      return message.channel.send(embedA)
-    if (!member) return message.reply(`Mencione o usuario a ser punido por favor.`)
-    const time = args[1];
-    if (!time) return message.reply(`Informe o tempo de mute **2m,7d**`)
+      .setTimestamp(new Date());
+
+    const confirmaçãoMutado = new Discord.MessageEmbed()
+      .setThumbnail(message.author.avatarURL({ dynamic: true, size: 1024 }))
+      .setTitle('Ação | Mute')
+      .setColor('#ff112b')
+      .setDescription(`\n<:Kaeltec:673592197177933864> **Staff:** ${message.author} \n**ID:** ${message.author.id}` + `\n<:Kaeltec:673592197177933864> **Mutado:** ${member.user.username} \n**ID:** ${member.id}` + `\n<:Registrado:673592197077270558> **Motivo:** ${reason}` + `\n<:KaelMutado:673592196972412949> **Tempo:** ${time}`)
+      .setFooter('🧁・Discord da Jeth', message.guild.iconURL({ dynamic: true, size: 1024 }))
+      .setTimestamp(new Date());
+
     let reason = args.slice(2).join(' ')
+
+    let muteRole = message.guild.roles.cache.find(r => r.name === 'Muted');
+
+    if (!message.member.hasPermission('KICK_MEMBERS'))
+      return message.channel.send(erroDePermissão)
+
+    if (!member) return message.reply(`Mencione o usuario a ser punido por favor.`)
+
+    if (!time) return message.reply(`Informe o tempo de mute **2m,7d**`)
+
     if (!reason) {
       reason = `Motivo: Não especificado.`
     }
-    let muteRole = message.guild.roles.cache.find(r => r.name === 'Muted');
+
+    if (message.member.roles.highest.position < message.guild.member(member).roles.highest.position) return message.reply(`Você não pode mutar esse usuario.`)
+
     if (!muteRole) muteRole = await message.guild.roles.create({
       data: {
         name: 'Muted',
@@ -46,15 +66,8 @@ module.exports = class mute extends Command {
       })
     });
 
-    if (message.member.roles.highest.position < message.guild.member(member).roles.highest.position) return message.reply(`Você não pode mutar esse usuario.`)
-    const embed = new Discord.MessageEmbed()
-      .setThumbnail(message.author.avatarURL({ dynamic: true, size: 1024 }))
-      .setTitle('Ação | Mute')
-      .setColor('#ff112b')
-      .setDescription(`\n<:Kaeltec:673592197177933864> **Staff:** ${message.author} \n**ID:** ${message.author.id}` + `\n<:Kaeltec:673592197177933864> **Mutado:** ${member.user.username} \n**ID:** ${member.id}` + `\n<:Registrado:673592197077270558> **Motivo:** ${reason}` + `\n<:KaelMutado:673592196972412949> **Tempo:** ${time}`)
-      .setFooter('🧁・Discord da Jeth', message.guild.iconURL({ dynamic: true, size: 1024 }))
-      .setTimestamp(new Date());
     if (member.roles.has(muteRole.id)) return message.channel.send('O usuário já se encontra mutado.')
+
     const Mutado = new this.client.database.mutado({
       _id: member.id,
       server: message.guild.id,
@@ -63,7 +76,7 @@ module.exports = class mute extends Command {
     })
 
     Mutado.save().then(() => {
-      message.channel.send(embed)
+      message.channel.send(confirmaçãoMutado)
       member.roles.add(muteRole.id)
     })
   }
