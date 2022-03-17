d29c4595547f2e1cd40c2c18b767e61a6b622fa8
RootGC_ <matheusbzo@hotmail.com>
2022-03-17 18:18:03 -0300

    Cleaning codes

--- src/commands/Fun/DogCommand.js
+++ src/commands/Fun/DogCommand.js
@@ -11,14 +11,15 @@ module.exports = class dog extends Command {
   }
 
   async run(message) {
-
     const gifs = ['https://media3.giphy.com/media/1qTIW3DqTPwUo/giphy.gif', 'https://media3.giphy.com/media/dYGzbKNV5Fh7vQwR4B/giphy.gif', 'https://i.pinimg.com/originals/50/bb/0a/50bb0aad7cd73787d6c2a9f3ab91d49a.gif', 'https://media1.tenor.com/images/95ad8741e29f32fb15487657d4d02082/tenor.gif?itemid=13072116'];
+
     const embed = new MessageEmbed()
       .setTitle(`Seu doguinho 😍`)
       .setImage(gifs[Math.floor(Math.random() * gifs.length)])
       .setColor(colors.default)
       .setTimestamp(new Date())
       .setFooter('🧁・Discord da Jeth', message.guild.iconURL({ dynamic: true, size: 1024 }))
+
     message.channel.send({ embeds: [embed] });
   }
 }
