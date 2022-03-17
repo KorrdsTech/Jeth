3f2679c051b3d2818658a0601f26151d510fac9d
RootGC_ <matheusbzo@hotmail.com>
2022-03-17 18:20:17 -0300

    Cleaning codes

--- src/commands/Fun/HighFiveCommand.js
+++ src/commands/Fun/HighFiveCommand.js
@@ -13,7 +13,8 @@ module.exports = class highfive extends Command {
   async run(message) {
     const user = message.mentions.users.first();
     const gifs = ['https://lh6.googleusercontent.com/hIRp_xCGjt6x5H5GSu9odKA9WPagzrMYPtT-Ow-Nte0AeHoMY4MUTlnxrZkJK248JAqNiBVi_9iaU3eYS2bWXtcdJFjsnrAV8i2H_iN5pjWWHDN6djKm2E-h3MQMUvM2DkoO3M7e.gif', 'https://c.tenor.com/JBBZ9mQntx8AAAAC/anime-high-five.gif', 'https://thumbs.gfycat.com/BreakableMessyHarrierhawk-size_restricted.gif'];
-    const falas = [`${message.author} **Deu um HIGH-FIVE com** ${user}`];
+    const falas = [`${message.author} **Deu um HIGH-FIVE com* ${user}`];
+
     const embed = new MessageEmbed()
       .setDescription(falas[Math.floor(Math.random() * falas.length)])
       .setImage(gifs[Math.floor(Math.random() * gifs.length)])
