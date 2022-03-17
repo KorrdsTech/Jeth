e4342d1ba5ab76381c56baea78114e314278add4
RootGC_ <matheusbzo@hotmail.com>
2022-03-17 18:59:57 -0300

    Cleaning codes

--- src/commands/Magic/DBClearCommand.js
+++ src/commands/Magic/DBClearCommand.js
@@ -11,12 +11,12 @@ module.exports = class dbclear extends Command {
   }
 
   async run(message) {
-
     this.client.guilds.cache.forEach(g => {
       this.client.database.guild.getAndDelete(g.id).then(() => {
         message.channel.send(`${g.name} deletada....`)
       })
     })
+
     this.client.users.cache.forEach(u => {
       this.client.database.user.getAndDelete(u.id).then(() => {
         message.channel.send(`${u.tag} deletado...`)
