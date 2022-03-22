beb7eee1dd5764b2b61e398ce7cd7e18c24cf822
DanielaGC <danielagc676@gmail.com>
2022-03-22 00:46:45 -0300

    Fix guildCreate

--- src/listeners/guildCreate.js
+++ src/listeners/guildCreate.js
@@ -1,4 +1,3 @@
 module.exports = async function onGuildCreate(guild) {
-  const server = await this.database.guild.getOrCreate(guild.id)
-  server.save()
+  await this.database.guild.getOrCreate(guild.id)
 }
