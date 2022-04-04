cdaf75b97004d3c3f67f51d3061bc6ef11416d08
RootGC_ <matheusbzo@hotmail.com>
2022-04-04 18:54:51 -0300

    Changing where bug report will be sent

--- src/commands/Misc/BugCommand.js
+++ src/commands/Misc/BugCommand.js
@@ -14,6 +14,7 @@ module.exports = class bug extends Command {
   async run(message, args) {
     const dono = process.env.OWNERS?.trim().split(',')
     const report = args.join(' ');
+    const logs = this.client.channels.cache.get('831041529825067038')
     if (!args[3]) return message.reply('<a:astaff:671435205302681603> `Err! Explique aqui detalhadamente o bug encontrado, ele será reportado diretamente para o coder do bot.`')
 
     const embed = new MessageEmbed()
@@ -28,6 +29,7 @@ module.exports = class bug extends Command {
     dono.forEach((ownerID) => {
       this.client.users.cache.get(ownerID).send({ embeds: [embed] }).catch(console.error)
     })
+    await logs.send({ embeds: [embed] })
     message.reply(`Seu ticket foi enviado com sucesso!`);
   }
 };
