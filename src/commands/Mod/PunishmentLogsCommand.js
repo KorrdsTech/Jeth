25a45e775f7c1f8d33ce7a64eef9e75b20d89253
RootGC_ <matheusbzo@hotmail.com>
2022-04-04 21:39:27 -0300

    Making small changes on moderation commands

---
+++ src/commands/Mod/PunishmentLogsCommand.js
@@ -0,0 +1,36 @@
+const { Command, colors } = require('../../utils')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class Ban extends Command {
+  constructor(name, client) {
+    super(name, client)
+
+    this.name = 'ban'
+    this.aliases = ['ban', 'banir', 'vaza', 'some']
+    this.category = 'Mod'
+  }
+
+  async run(message, args) {
+    const emptyMessage = new MessageEmbed()
+      .setColor(colors['mod'])
+      .setTitle('<:plus:955577453441597550> **PunishmentLogs:**', `${message.author.username}`, true)
+      .setDescription('Criado para facilitar o envio de logs dentro de seu servidor, desta forma facilitando o gerenciamento de auditoria de seu servidor.') // inline false
+      .addField('*Uso do comando:*', '`punishmentlogs set <#canal>`', true)
+      .addField('*Uso do comando:*', '`punishmentlogs remove <#canal>`', true)
+      .addField('*Exemplo:*', '`punishmentlogs set #geral`', true)
+
+    if (!args[0]) return message.reply({ embeds: [emptyMessage] })
+
+    const guildDocument = await this.client.database.Guilds.findById(message.guild)
+    if (args[0] === 'set') {
+      guildDocument.punishChannel = args[1]
+      guildDocument.save()
+      return (0);
+    }
+    else if (args[0] === 'remove') {
+      guildDocument.punishChannel = ''
+      guildDocument.save()
+      return (0);
+    }
+  }
+}
