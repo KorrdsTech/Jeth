1d84cb77f421f6bbec60825a73dac92f6f42b297
RootGC_ <matheusbzo@hotmail.com>
2022-05-29 21:47:33 -0300

    Fixing ident and sending to host

--- src/commands/VIP/VIPCommand.js
+++ src/commands/VIP/VIPCommand.js
@@ -112,7 +112,7 @@ module.exports = class VipCommand extends Command {
 
         msg.delete()
         setTimeout(() => {
-         message.reply({ content: `${emojis.check} » Dashboard [Vip] de ${message.author} foi fechado. (Automático)` })
+          message.reply({ content: `${emojis.check} » Dashboard [Vip] de ${message.author} foi fechado. (Automático)` })
         }, 1000)
       });
 
