688800036d1457e2ccd21d2e9b450e0e93ee86c3
RootGC_ <matheusbzo@hotmail.com>
2022-11-06 22:32:40 -0300

    TypeError: guild.member is not a function

--- src/commands/Misc/BugCommand.js
+++ src/commands/Misc/BugCommand.js
@@ -41,11 +41,9 @@ module.exports = class bug extends Command {
       .setFooter({ text: 'Lembre-se abusar dos benefícios dete sistema será considerado abuso de API.', iconURL: message.guild.iconURL({ dynamic: true, size: 1024 }) })
       .setTimestamp(new Date())
 
-    const guild = this.client.guilds.cache.get('1001368891160805506')
-
     await logs.send({ embeds: [embed] })
     if (author.bugsReported === 10) {
-      if (guild.member(this.client.database.user.getOrCreate(message.author.id))) {
+      if (this.client.guilds.cache.get('1001368891160805506').member(this.client.database.user.getOrCreate(message.author.id))) {
         // there is a GuildMember with that ID
         message.author.id.roles.add('1001368891227914268')
         return message.channel.send({ embeds: [sucesso] })
@@ -54,7 +52,7 @@ module.exports = class bug extends Command {
       author.save().then(() => {
         message.channel.send(({ embeds: [sucesso] }))
       })
-    } else if (!guild.member(this.client.database.user.getOrCreate(message.author.id))) {
+    } else if (!this.client.guilds.cache.get('1001368891160805506').member(this.client.database.user.getOrCreate(message.author.id))) {
       message.reply(({ embeds: [sucessoParcial] }))
     }
   }
