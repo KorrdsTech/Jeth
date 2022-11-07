31f0005b7df86d4e6e1e251baaea25e1776a4f87
RootGC_ <matheusbzo@hotmail.com>
2022-11-07 16:31:40 -0300

    Releasing fixes at messageCreate on weblinks

--- src/listeners/messageCreate.js
+++ src/listeners/messageCreate.js
@@ -27,7 +27,7 @@ module.exports = async function onMessage(message) {
         .setLabel('Suporte')
         .setStyle('LINK')
         .setEmoji('<:b_blurpleemployee:856174396423274516>')
-        .setURL('https://discord.gg/jeth')
+        .setURL('https://discord.gg/WPUYahyPzx')
     )
 
     message.channel.send({ content: `<a:b_hypesquadi:887899688634839042> **Olá, **${message.author}! Prazer em ter você utilizando nossos comandos, tem algo em que eu possa ajudar? Caso queira saber os meus comandos, por favor use ${guildDocument.prefix}ajuda que lhe enviarei tudo sobre meus comandos! <a:a_dancin:934175860930527313> Atualmente possuo **${totalCommands}** comandos!`, components: [row] })
