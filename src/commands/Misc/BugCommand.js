7edb8db4712ab90c77456ea3de45c8241d36890a
RootGC_ <matheusbzo@hotmail.com>
2022-11-07 00:05:19 -0300

    Fixing embed issue

--- src/commands/Misc/BugCommand.js
+++ src/commands/Misc/BugCommand.js
@@ -58,21 +58,20 @@ module.exports = class bug extends Command {
           logs.send({ embeds: [embed] })
           message.channel.send({ embeds: [sucesso] })
         })
-      } else if (author.bugsReported === 9) {
-        message.author.roles.add('1001368891227914268').then(() => {
-          message.author.send(({ embeds: [cargoEmbed] }))
-          logs.send({ embeds: [embed] })
-          message.channel.send(({ embeds: [sucesso] }))
-          author.bugsReported += 1
-          author.save()
-        })
-      } else if (!guild.members.cache.get(message.author.id)) {
-        logs.send({ embeds: [embed] })
-        await message.channel.send(({ embeds: [sucessoParcial] }))
-      } else if (author.bugsReported < 10) {
+      } else if (author.bugsReported >= 10) {
         logs.send({ embeds: [embed] })
         await message.channel.send({ embeds: [sucesso] })
       }
+    } else if (author.bugsReported === 9) {
+      message.author.roles.add('1001368891227914268')
+      message.author.send({ embeds: [cargoEmbed] })
+      logs.send({ embeds: [embed] })
+      message.channel.send({ embeds: [sucesso] })
+      author.bugsReported += 1
+      author.save()
+    } else if (!guild.members.cache.get(message.author.id)) {
+      logs.send({ embeds: [embed] })
+      await message.channel.send(({ embeds: [sucessoParcial] }))
     }
   }
 };
