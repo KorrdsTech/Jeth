2f498a9583eeda6a1373cfbc532900231fc2b6b3
RootGC_ <matheusbzo@hotmail.com>
2022-11-12 02:14:57 -0300

    Fixing id changes

--- src/commands/Mod/WelcomeModuleCommand.js
+++ src/commands/Mod/WelcomeModuleCommand.js
@@ -143,20 +143,20 @@ module.exports = class welcomeModule extends Command {
 
       let embedCount = 1
       message.reply({ embeds: [embed] }).then(async m => {
-        await m.react('666762183249494027')// ir para frente
-        const filter = (e, u) => (u.id == message.author.id) & (e.emoji.id == '666762183249494027' || e.emoji.id == '665721366514892839')
+        await m.react('1040856493920096286')// ir para frente
+        const filter = (e, u) => (u.id == message.author.id) & (e.emoji.id == '1040856493920096286' || e.emoji.id == '665721366514892839')
         const col = m.createReactionCollector({ filter, time: 180_000, errors: ['time'] })
         col.on('collect', async (e) => {
-          if (embedCount != 2 && e.emoji.id == '666762183249494027') { // ir para frente
+          if (embedCount != 2 && e.emoji.id == '1040856493920096286') { // ir para frente
 
-            await m.react('665721366514892839')
+            await m.react('◀️')
             e.users.cache.map(u => e.remove(u.id))
             m.edit({ embeds: [embed2] })
             embedCount = 2
-            await m.react('665721366514892839')// volta para trás
-          } else if (e.emoji.id == '665721366514892839' && embedCount == 2) {
+            await m.react('◀️')// volta para trás
+          } else if (e.emoji.id == '◀️' && embedCount == 2) {
 
-            await m.react('666762183249494027')
+            await m.react('1040856493920096286')
             e.users.cache.map(u => e.remove(u.id))
 
             m.edit({ embeds: [embed] })
