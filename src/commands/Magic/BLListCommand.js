0435693e4e7af7fc3d10ab157eb48980ae451503
RootGC_ <matheusbzo@hotmail.com>
2022-11-22 00:18:02 -0300

    Fixing small error >> 6.1.1

--- src/commands/Magic/BLListCommand.js
+++ src/commands/Magic/BLListCommand.js
@@ -14,7 +14,7 @@ module.exports = class blList extends Command {
   async run(message, args) {
     const staff = await this.client.database.user.getOrCreate(message.author.id)
     if (!staff.staff) {
-      return message.reply('Você não pode utilizar este comando, somente os membros confiados da equipe <@&718178715426619489>')
+      return message.reply('Você não pode utilizar este comando, somente os membros confiados da equipe <@&1041559731619233804>')
     }
     const userDoc = await this.client.database.user.model.find({ 'blacklist': true })
     const msg = [];
