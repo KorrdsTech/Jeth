a0ec51cabecd89c1797dd2b4a17a58876e1a2ff7
RootGC_ <matheusbzo@hotmail.com>
2022-12-29 01:30:52 -0300

    Sending patch updates

--- src/commands/Misc/ProfileCommand.js
+++ src/commands/Misc/ProfileCommand.js
@@ -1,12 +1,16 @@
 const { Command } = require('../../utils')
 const { MessageAttachment } = require('discord.js')
 const { loadImage, registerFont, createCanvas } = require('canvas');
-registerFont('src/assets/fonts/Montserrat-Black.ttf', { family: 'Montserrat' });
-registerFont('src/assets/fonts/Segoe Print.ttf', { family: 'Segoe Print' });
-registerFont('src/assets/fonts/Segoe UI.ttf', { family: 'Segoe UI' });
-registerFont('src/assets/fonts/Segoe UI Black.ttf', {
-  family: 'Segoe UI Black',
+registerFont('src/assets/fonts/Minecraft.ttf', {
+  family: 'Minecraft',
 });
+registerFont('src/assets/fonts/Sugar Snow.ttf', {
+  family: 'Sugar Snow',
+});
+registerFont('src/assets/fonts/my-standard.ttf', {
+  family: 'my-standard',
+});
+
 const util = require('../../utils/Util')
 
 const Utils = new util();
@@ -21,7 +25,10 @@ module.exports = class Profile extends Command {
   }
 
   async run(message, args) {
+    const jeth = this.client.guilds.cache.get('1001368891160805506')
+    const pUser = message.guild.members.cache.get(args[0]?.replace(/[<@!>]/g, '') || message.author.id)
     const documento = await this.client.database.guild.getOrCreate(message.guild.id)
+    const doc = await this.client.database.user.getOrCreate(pUser)
     const prefix = documento.prefix
     const USER =
             this.client.users.cache.get(args[0]) ||
@@ -31,13 +38,16 @@ module.exports = class Profile extends Command {
     const user = await this.client.database.user.getOrCreate({ _id: USER.id });
     const canvas = createCanvas(900, 600);
     const ctx = canvas.getContext('2d');
+    const member1 = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.author;
+    if (!member1) return message.reply('Algum erro terrível aconteceu, entre em contato com o suporte via /bug')
+    const user1 = await this.client.database.user.getOrCreate(member1.id)
 
         //========================// Import Avatar //========================//
 
     const avatar = await loadImage(
       USER.displayAvatarURL({ format: 'jpeg', size: 2048 })
     );
-    ctx.drawImage(avatar, 20, 90, 195, 180);
+    ctx.drawImage(avatar, 20, 100, 220, 320);
 
         //========================// Import Background //========================//
 
@@ -73,91 +83,117 @@ module.exports = class Profile extends Command {
         // Username
 
     ctx.textAlign = 'left';
-    ctx.font = '50px "Segoe UI Black"';
+    ctx.font = '30px "Minecraft"';
     ctx.fillStyle = 'rgb(253, 255, 252)';
-    ctx, this.shorten(USER.username, 20), 230, 190;
-    await Utils.renderEmoji(ctx, this.shorten(USER.username, 20), 230, 190);
+    ctx, this.shorten(USER.username, 30), 290, 270;
+    await Utils.renderEmoji(ctx, this.shorten(USER.username, 30), 315, 240);
         // Badges
 
     let list = [];
 
     const member = message.guild.members.cache.get(USER.id)
-    if (member && member.premiumSinceTimestamp) list.push('BOOSTER')
-    if (user.isMarried) list.push('CASADO');
-    if (USER.id === process.env.OWNERS) list.push('DONO');
+    if (process.env.OWNERS?.includes(message.author.id)) list.push('CREATOR');
     if (message.guild.owner?.id === USER.id) list.push('SERVER_OWNER');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1038349301274923018')) list.push('STAFF');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1041559731619233804')) list.push('TRUST_SAFETY');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1001368891240480778')) list.push('JETH_PARTNER');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1050932461049221210')) list.push('DEV');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1001368891227914268')) list.push('BUG_HUNTER');
+    if (member && member.premiumSinceTimestamp) list.push('BOOSTER');
     if (user.vip === true) list.push('VIP');
+    if (user.isMarried) list.push('CASADO');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1038339614072655882')) list.push('PATREON');
+
     const mapper = {
 
-      HOUSE_BRAVERY: '<:Bravery:938280523153965097>',
-      HOUSE_BRILLIANCE: '<:Brilliance:938280523426578533>',
-      HOUSE_BALANCE: '<:Balance:938280523409817600>',
-      EARLY_VERIFIED_BOT_DEVELOPER: '<:ThisUserIsJethDev:939896774267654264>',
-      BOOSTER: '<:BadgeServerBoostLevel3:1030872148153225266>',
-      SERVER_OWNER: '<:owner:931333021557227540>'
+      TRUST_SAFETY: '<:IconCrown:1030872512847945798>',
+      SERVER_OWNER: '<:IconCrown:1030872512847945798>',
+      CREATOR: '<:ThisUserIsCEO:938280523585957978>',
+      HOUSE_BRAVERY: '<:HeroHypeRenwil:1052599369750290484>',
+      HOUSE_BRILLIANCE: '<:HeroHypeUrdim:1030872161902137459>',
+      HOUSE_BALANCE: '<:HeroHypeArame:1030872158940975284>',
+      DEV: '<a:activedeveloperbadgeanimated:1052605173299683411>',
+      BOOSTER: '<:BadgeNitroBoosting_1Month:1030872129731829790>',
+      BUG_HUNTER: '<:BadgeDiscordBugHunterGold:1030872109070680084>',
+      PATREON: '<a:gift4patreon:1057797332881641503>',
+      JETH_PARTNER: '<:ThisUserIsJethPartner:938282680028651600>',
+      STAFF: '<:BadgeDiscordStaff:1030872112841367614>',
+
     }
 
-    USER.flags.toArray().map(flag => { list.push(mapper[flag] ?? 'Sem flag') } )
+    USER.flags.toArray().map(flag => { list.push(mapper[flag] ?? '') } )
     list = list
 
       .join(',')
 
-      .replace('EARLY_VERIFIED_BOT_DEVELOPER', '<:ThisUserIsJethDev:939896774267654264>')
-      .replace('HOUSE_BRAVERY', '<:Bravery:938280523153965097>')
-      .replace('HOUSE_BRILLIANCE', '<:Brilliance:938280523426578533>')
-      .replace('HOUSE_BALANCE', '<:Balance:938280523409817600>')
-      .replace('BOOSTER', '<:BadgeServerBoostLevel3:1030872148153225266>')
+      .replace('CREATOR', '<:ThisUserIsCEO:938280523585957978>')
+      .replace('SERVER_OWNER', '<:IconCrown:1030872512847945798>')
+      .replace('HOUSE_BRAVERY', '<:HeroHypeRenwil:1052599369750290484>')
+      .replace('HOUSE_BRILLIANCE', '<:HeroHypeUrdim:1030872161902137459>')
+      .replace('HOUSE_BALANCE', '<:HeroHypeArame:1030872158940975284>')
+      .replace('DEV', '<a:activedeveloperbadgeanimated:1052605173299683411>')
+      .replace('VIP', '<:Star:1047262776625725460>')
+      .replace('BOOSTER', '<:BadgeNitroBoosting_1Month:1030872129731829790>')
       .replace('CASADO', '💍')
-      .replace('DONO', '<:ThisUserIsCEO:938280523585957978>')
-      .replace('SERVER_OWNER', '<:owner:931333021557227540>')
-      .replace('VIP', '<:ThisUserIsVIP:938280523468509245>');
+      .replace('BUG_HUNTER', '<:BadgeDiscordBugHunterGold:1030872109070680084>')
+      .replace('JETH_PARTNER', '<:ThisUserIsJethPartner:938282680028651600>')
+      .replace('TRUST_SAFETY', '<:Certified_Moderator:1047259863148941373>')
+      .replace('PATREON', '<a:gift4patreon:1057797332881641503>')
+      .replace('STAFF', '<:BadgeDiscordStaff:1030872112841367614>')
 
-    ctx.font = `30px "Segoe Print"`;
+    ctx.font = `30px "Minecraft"`;
 
-    await Utils.renderEmoji(ctx, list.split(',').join(' '), 230, 240);
+    await Utils.renderEmoji(ctx, list.split(',').join(' '), 35, 55)
 
         // Titles
 
     ctx.textAlign = 'left';
-    ctx.font = '30px "Segoe UI Black"';
+    ctx.font = '30px "Minecraft"';
     ctx.fillStyle = 'rgb(253, 255, 252)';
-    ctx.fillText('Coins', 600, 90);
-    ctx.fillText('XP', 600, 155);
+    ctx.fillText('Coins:', 315, 342);
+    ctx.fillText('XP:', 315, 395);
     ctx.textAlign = 'center';
-    ctx.font = '20px "Segoe UI Black"';
+    ctx.font = '20px "Minecraft"';
     if (user.isMarried) {
-      ctx.font = '30px "Segoe UI Black"';
+      ctx.font = '20px "Minecraft"';
       ctx.fillStyle = 'rgb(253, 255, 252)';
-      ctx.fillText('Casado(a) com:', 710, 220);
-      ctx.font = '20px "Segoe UI Black"';
+      ctx.fillText('Casado(a) com:', 350, 480);
+      ctx.font = '15px "Minecraft"';
       ctx.fillText(
         await this.client.users.fetch(user.userMarried).then((x) => x.tag),
-        710,
-        250
+        350,
+        500
       );
     }
+
+        // Reps
+    ctx.textAlign = 'right';
+    ctx.font = '40px "Minecraft"';
+    ctx.fillStyle = 'rgb(253, 255, 252)';
+    ctx.fillText(`${user1.rep}`, 220, 515);
+
         // Coins/XP
     ctx.textAlign = 'left';
-    ctx.font = '30px "Segoe UI"';
+    ctx.font = '30px "my-standard"';
     ctx.fillStyle = 'rgb(253, 255, 252)';
-    ctx.fillText(`${user.coins}`, 600, 120);
+    ctx.fillText(`${user.coins}`, 415, 342);
     ctx.fillText(
       `#${user.level} | ${user.xp}/${(
         user.nextLevel
       )}`,
-      600,
-      185
+      280,
+      442
     );
 
         // Sobre
 
-    ctx.font = '20px "Montserrat"';
+    ctx.font = '20px "my-standard"';
     ctx.fillText(
       user.about == 'null'
-        ? `Use ${prefix}sobremim <msg> para alterar essa mensagem`
-        : user.about.match(/.{1,50}/g).join('\n'),
-      160,
-      350
+        ? `Use ${prefix}sobremim para alterar essa mensagem`
+        : user.about.match(/.{1,25}/g).join('\n'),
+      480,
+      365
     );
 
         //========================// Create Image //========================//
